89e2a20762fc75370dd7746442557def5657fc27
temch584 <temch584@student.otago.ac.nz>
2024-04-09 00:11:53 +1200

    Created Initial tables for databases and added some variables

---
+++ sql/dp_setup.sql
@@ -0,0 +1,43 @@
+-- create user
+CREATE USER 'INFO310' IDENTIFIED BY 'password';
+GRANT ALL ON *.* TO 'INFO310' WITH GRANT OPTION;
+
+-- create database
+DROP TABLE IF EXISTS Room;
+DROP DATABASE INFO310PROJECT;
+
+CREATE DATABASE INFO310PROJECT;
+USE INFO310PROJECT;
+
+CREATE TABLE Room(
+    room_id INT AUTO_INCREMENT PRIMARY KEY,
+    threshold INT NOT NULL
+);
+
+CREATE TABLE RoomTime(
+    room_id INT NOT NULL,
+    CONSTRAINT fk_roomtime FOREIGN KEY (room_id) REFERENCES Room(room_id),
+    occupancy INT NOT NULL,
+    time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    date DATE NOT NULL,
+    PRIMARY KEY (room_id, date, time)
+);
+
+CREATE TABLE UserTimes(
+    user_id INT,
+    room_id INT,
+    room_threshold INT NOT NULL,
+    start_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    end_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_usertimes_userid FOREIGN KEY(user_id) REFERENCES User(user_id),
+    CONSTRAINT fk_usertimes_roomid FOREIGN KEY(room_id) REFERENCES Room(room_id),
+    PRIMARY KEY(user_id, room_id);
+);
+
+CREATE TABLE User(
+);
+
+CREATE TABLE Login(
+);
+
