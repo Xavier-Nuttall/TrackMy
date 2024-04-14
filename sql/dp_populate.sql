adeb277a01e989e1daf7932f8e62292f13c0efe9
albme224 <albme224@student.otago.ac.nz>
2024-04-14 20:20:31 +1200

    added a few room and user inputs - need2study4test

--- sql/dp_populate.sql
+++ sql/dp_populate.sql
@@ -1,8 +1,21 @@
 -- ROOM INSERT STATEMENTS
 INSERT INTO Room (threshold, room_name) VALUES (10, 'Conference Room A');
+INSERT INTO Room (threshold, room_name) VALUES (300, 'Not A Giant Pit');
+INSERT INTO Room (threshold, room_name) VALUES (20, 'Study Room A');
+INSERT INTO Room (threshold, room_name) VALUES (25, 'Study Room B');
+INSERT INTO Room (threshold, room_name) VALUES (30, 'Study Room C');
+INSERT INTO Room (threshold, room_name) VALUES (50, 'Weights Room');
+INSERT INTO Room (threshold, room_name) VALUES (10, 'Indoor Gymnasium 1');
+INSERT INTO Room (threshold, room_name) VALUES (10, 'Indoor Gymnasium 2');
 
 --USER INSERT STATEMENTS
 INSERT INTO User (email_address, firstname, lastname, othernames) VALUES ('john.doe@example.com', 'John', 'Doe', NULL);
+INSERT INTO User (email_address, firstname, lastname, othernames) VALUES ('jane.doe@example.com', 'Jane', 'Doe', NULL);
+INSERT INTO User (email_address, firstname, lastname, othernames) VALUES ('lbowman@name.gen', 'Lacey', 'Bowman', NULL);
+INSERT INTO User (email_address, firstname, lastname, othernames) VALUES ('pwhite@name.gen', 'Paul', 'White', NULL);
+INSERT INTO User (email_address, firstname, lastname, othernames) VALUES ('aking@public.domain', 'Arthur', 'King', NULL);
+INSERT INTO User (email_address, firstname, lastname, othernames) VALUES ('mycroft@gov.uk', 'Mycroft', 'Holmes', NULL);
+INSERT INTO User (email_address, firstname, lastname, othernames) VALUES ('sherlock@public.domain', 'Sherlock', 'Holmes', NULL);
 
 --USERTIMES INSERT STATEMENTS
 INSERT INTO UserTimes (user_id, room_id, room_threshold, start_time, end_time) VALUES (1, 1, 10, '2024-04-10 08:00:00', '2024-04-10 10:00:00');
