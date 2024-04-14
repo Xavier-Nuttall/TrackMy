025759339500cd99493040e5c9ed911745564a24
Xavier Nuttall <nutxa216@student.otago.ac.nz>
2024-04-14 21:28:11 +1200

    db.js linked to database

--- backend/db/db.js
+++ backend/db/db.js
@@ -6,10 +6,10 @@ const { Pool } = require('pg');
  */
 const pool = new Pool({
     host: 'localhost', 
-    user: 'info310',
-    password: 'password',
+    user: 'admin',
+    password: 'pw',
     database: 'trackmy',
-    port: 5432,                // Default port for PostgreSQL
+    port: 8000,                // Default port for PostgreSQL
     max: 10,                   // Set the max number of clients in the pool
     idleTimeoutMillis: 30000,  // Close idle clients after 30 seconds
     connectionTimeoutMillis: 2000,  // Return an error after 2 seconds if connection cannot be established
