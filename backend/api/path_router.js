3454b88ea320a1de50b2f39d9a0e8adcc5b4e2f8
Englishbj17 <60582673+Englishbj17@users.noreply.github.com>
2024-04-22 00:33:08 +1200

    Created basic paths in router

--- backend/api/path_router.js
+++ backend/api/path_router.js
@@ -14,4 +14,43 @@ router.get('/trackmy/', async (req, res) => {
     res.status(404).send("oops");
 });
 
+// User log in page
+router.get('/login/', async (req, res) => {
+    res.render('login')
+})
+
+// Home page after users have logged in
+router.get('/', async (req, res) => {
+    res.render('home_before-login');
+});
+
+// notifications site
+router.get('/notifications/', async (req, res) => {
+    res.render('notifs');
+});
+
+// Profile page
+router.get('/Profile/', async (req, res) => {
+    res.render('profile');
+});
+
+// about page
+router.get('/About/', async (req, res) => {
+    res.render('about');
+});
+
+// Room information
+router.get('/Rooms/', async (req, res) => {
+    //retrieves data from sql for information on all the rooms
+    //const rooms = await pool.query('SELECT * FROM rooms');
+    res.render('rooms', );
+});
+
+// Specific Room Information
+router.get('/Room-Information/', async (req, res) => {
+    //const roomId = req.params.roomId;
+    //const roomData = await pool.query(SQL statement to get a specific rooms id);
+    res.render('Specific_room_info');
+});
+
 module.exports = router; 
