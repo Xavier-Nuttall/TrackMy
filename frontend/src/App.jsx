0f1edb3005e0fc96a1e3352d3b085b143526f242
temch584 <temch584@student.otago.ac.nz>
2024-04-24 00:25:52 +1200

    front end changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import WebSocketComponent from './websocketcomponent';
 
@@ -88,47 +87,65 @@ function AccountPage() {
 
 function NotifPage() {
   return (
-    <main>
-      {/* Comment: probably a minimap on the side of the floor plan to have a reference to which room you're going for */}
-      {/* Comment: with the floor change */}
-      <div className="grid-contain">
-        <div id="top-set">
-          <div id="new-notif">
-            <fieldset>
-              <legend>New Notification</legend>
-              <form>
-                <label>Room:</label>
-                <select name="room">
-                  <option>Example Room</option>
-                </select>
-                <label>Notify when occupancy goes over this amount of people:</label><input type="number" name="threshold" />
-                <label>When to begin receiving notifications:</label><input type="time" name="startTime" />
-                <label>Notifications will stop being received after:</label><input type="time" name="endTime" />
-                <input type="submit" value="Create New" />
-              </form>
-            </fieldset>
+      <main>
+        {/* Probably a minimap on the side of the floor plan to have a reference to which room you're going for */}
+        {/* . with the floor change */}
+        <div className="grid-contain">
+          <div id="top-set">
+            <div id="new-notif">
+              <fieldset>
+                <legend>New Notification</legend>
+                <form>
+                  <label htmlFor="room">Room:</label>
+                  <select id="room">
+                    <option>Example Room</option>
+                  </select>
+                  <label htmlFor="threshold">Notify when occupancy goes over this amount of people:</label>
+                  <input id="threshold" type="number" />
+                  <label htmlFor="startTime">When to begin receiving notifications:</label>
+                  <input id="startTime" type="time" />
+                  <label htmlFor="endTime">Notifications will stop being received after:</label>
+                  <input id="endTime" type="time" />
+                  <input type="submit" value="Create New" />
+                </form>
+              </fieldset>
+            </div>
+            <div id="mini-map">
+              <p>heatmap goes here albeit without current stats.<br />primarily for reference when making notifs</p>
+            </div>
           </div>
-          <div id="mini-map">
-            <p>heatmap goes here albeit without current stats.<br />primarily for reference when making notifs</p>
-          </div>
-        </div>
-        <div id="full-list">
-          <h3>Current Notifications</h3>
-          <dl id="list-notifs">
-            <dt>Room A</dt>
-            <dd>Threshold: 10</dd>
-            <dd>Times: 10:00 - 18:00</dd>
-            <dd><button>Delete Notification</button></dd>
+          <div id="full-list">
+            <h3>Current Notifications</h3>
+            <dl id="list-notifs">
+              <dt>Room A</dt>
+              <dd>Threshold: 10</dd>
+              <dd>Times: 10:00 - 18:00</dd>
+              <dd><button>Delete Notification</button></dd>
 
-            <dt>Room B</dt>
-            <dd>Threshold: 20</dd>
-            <dd>Times: 9:00 - 15:00</dd>
-            <dd><button>Delete Notification</button></dd>
-          </dl>
+              <dt>Room B</dt>
+              <dd>Threshold: 20</dd>
+              <dd>Times: 9:00 - 15:00</dd>
+              <dd><button>Delete Notification</button></dd>
+
+              <dt>Room B</dt>
+              <dd>Threshold: 20</dd>
+              <dd>Times: 9:00 - 15:00</dd>
+              <dd><button>Delete Notification</button></dd>
+
+              <dt>Room B</dt>
+              <dd>Threshold: 20</dd>
+              <dd>Times: 9:00 - 15:00</dd>
+              <dd><button>Delete Notification</button></dd>
+
+              <dt>Room B</dt>
+              <dd>Threshold: 20</dd>
+              <dd>Times: 9:00 - 15:00</dd>
+              <dd><button>Delete Notification</button></dd>
+            </dl>
+          </div>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
 }
 
 function App() {
@@ -136,7 +153,6 @@ function App() {
   const [menuOpen, setMenuOpen] = useState(true);
 
   const toggleFilter = () => {
-    console.log("Hello");
     setMenuOpen(!menuOpen);
   };
 
@@ -178,59 +194,32 @@ function App() {
   };
 
   return (
-    <html>
-      <head>
-        <title>TrackMy</title>
-      </head>
-      <body>
-        <div className={`content-container${menuOpen ? '2' : '1'}`}>
-          <div id="title">
-            <div id="menu" onClick={toggleFilter}>
-              <div id="menu-button" className={`menu-button ${menuOpen ? 'change' : ''}`} >
-                <div class="bar1"></div>
-                <div class="bar2"></div>
-                <div class="bar3"></div>
-              </div>
-            </div>
-            <h1>TrackMy</h1>
-            <div className="search-container">
-              <button className="search-button" onClick={toggleSearch}>
-                <span className="icon"></span>
-              </button>
-            </div>
+    <div className={`content-container${menuOpen ? '2' : '1'}`}>
+      <div id="title">
+        <div id="menu" onClick={toggleFilter}>
+          <div id="menu-button" className={`menu-button ${menuOpen ? 'change' : ''}`} >
+            <div className="bar1"></div>
+            <div className="bar2"></div>
+            <div className="bar3"></div>
           </div>
-          <nav className={menuOpen ? '' : 'hide-nav'}>
-            <div className="go" id="graph">
-              <button onClick={handleMainPageClick}>Home</button>
-            </div>
-            <div className="go" id="about">
-              <button onClick={handleAboutUsPageClick}>About Us</button>
-            </div>
-            <div className="go" id="login">
-              <button onClick={handleLoginPageClick}>Log In | Register</button>
-            </div>
-            <div className="go" id="settings">
-              <button onClick={handleAccountSettingsPageClick}>Account Settings</button>
-            </div>
-            <div className="go" id="notif">
-              <button onClick={handleNotifPageClick}>Edit Notifications</button>
-            </div>
-
-            <dl id="curr-notifs">
-              <dt>Room A</dt>
-              <dd>Threshold: 10</dd>
-              <dd>Times: 10:00 - 18:00</dd>
-
-              <dt>Room B</dt>
-              <dd>Threshold: 20</dd>
-              <dd>Times: 9:00 - 15:00</dd>
-            </dl>
-          </nav>
-
-          {mainContent}
         </div>
-      </body>
-    </html>
+        <h1>TrackMy</h1>
+        <div className="search-button" onClick={toggleSearch}>
+          <span className="icon"></span>
+        </div>
+      </div>
+      <nav className={`nav ${menuOpen ? 'open' : ''}`}>
+        <button className="sidebar-button" onClick={handleMainPageClick}>Home</button>
+        <button className="sidebar-button" onClick={handleAboutUsPageClick}>About Us</button>
+        <button className="sidebar-button" onClick={handleLoginPageClick}>Log In | Register</button>
+        <button className="sidebar-button" onClick={handleAccountSettingsPageClick}>Account Settings</button>
+        <button className="sidebar-button" onClick={handleNotifPageClick}>Edit Notifications</button>
+
+
+      </nav>
+
+      {mainContent}
+    </div>
   );
 }
 
