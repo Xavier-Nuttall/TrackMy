abf668d1c30df512cf7a817bdddb8362b2c795a7
albme224 <albme224@student.otago.ac.nz>
2024-04-24 14:43:19 +1200

    moved over floor select and ind graphs from mock

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,23 @@ import WebSocketComponent from './websocketcomponent';
 function HomePage() {
   return (
     <main>
-      <div className="grid-contain">
+      <div className="grid-contain" id="main-show">
         <div id="graph-show">
           <p>heatmap goes here</p>
           <WebSocketComponent></WebSocketComponent>
+          <div id="floor-select">
+              <dl>
+                  <dt>F3</dt>
+                  <dt>F2</dt>
+                  <dt>F1</dt>
+              </dl>
+          </div>
         </div>
         <div id="room-show">
           <p>individual room stats go here either via grid or list</p>
+        </div>
+        <div id="room-graph-show">
+            <p>this panel is for selecting a specific room and showing the trends for a set time frame. will also test js graphs here.</p>
         </div>
       </div>
     </main>
