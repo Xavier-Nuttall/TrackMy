1ea8e534f04630ff1220dd1d33fa14baf6ae50be
albme224 <albme224@student.otago.ac.nz>
2024-04-29 15:26:48 +1200

    started room info on home

--- frontend/src/view/home.jsx
+++ frontend/src/view/home.jsx
@@ -22,6 +22,22 @@ function HomePage() {
           </div>
           <div id="room-show">
             <p>this is room information for selected room in overhead panel heatmap. that functions as the room list. information here will be basic statistics for each room throughout the week</p>
+            <h1>Room X</h1>
+            <table>
+              <tr>
+                <th>Variable</th> {/**what do i call this anyways  */}
+                <th>Value</th>
+              </tr>
+              <tr>
+                <td>Current Present</td>
+              </tr>
+              <tr>
+                <td>Maximum this week</td>
+              </tr>
+              <tr>
+                <td>Minimum this week</td>
+              </tr>
+            </table>
           </div>
           
         </div>
