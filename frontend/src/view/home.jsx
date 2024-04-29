fe6d575a9627444d4f3f005f8beca3e475a4dbe2
albme224 <albme224@student.otago.ac.nz>
2024-04-29 14:19:57 +1200

    threw in example room list

--- frontend/src/view/home.jsx
+++ frontend/src/view/home.jsx
@@ -19,6 +19,25 @@ function HomePage() {
           </div>
           <div id="room-show">
             <p>individual room stats go here either via grid or list</p>
+            <div id="list-rooms">
+                <dl>
+                <dt>Room A</dt>
+                <dd>Threshold: 10</dd>
+                <dd><button>Room Stats</button></dd>
+  
+                <dt>Room B</dt>
+                <dd>Threshold: 20</dd>
+                <dd><button>Room Stats</button></dd>
+
+                <dt>Room C</dt>
+                <dd>Threshold: 100</dd>
+                <dd><button>Room Stats</button></dd>
+
+                <dt>Room D</dt>
+                <dd>Threshold: 5</dd>
+                <dd><button>Room Stats</button></dd>
+                </dl>
+            </div>
           </div>
           <div id="room-graph-show">
               <p>this panel is for selecting a specific room and showing the trends for a set time frame. will also test js graphs here.</p>
