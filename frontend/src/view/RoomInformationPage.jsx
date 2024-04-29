0b4eee5aa12276fd6d1b31afb9e91e55973c7ef6
temch584 <temch584@student.otago.ac.nz>
2024-04-29 14:27:59 +1200

    Corrected packages

--- frontend/src/view/RoomInformationPage.jsx
+++ frontend/src/view/RoomInformationPage.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import {Chart as ChartJS} from "chart.js/auto";
+import {Bar, Line} from "react-chartjs-2";
 
 function RoomInformationPage() {
     return (
@@ -6,6 +8,19 @@ function RoomInformationPage() {
         <div className="general-panel">
           <h1>Room information</h1>
           <p>This will be where all the graphs and such for each room will be</p>
+
+          <Line 
+            data = {{
+              labels: ["A", "B", "C"],
+              datasets: [
+                {
+                  label: "Revenue",
+                  data: [200, 300, 400],
+                }
+              ],
+
+            }}
+          />
         </div>
       </main>
     );
