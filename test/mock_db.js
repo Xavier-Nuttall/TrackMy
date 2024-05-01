99c63c018026a3251377a541b294e181cbe914cc
Xavier Nuttall <nutxa216@student.otago.ac.nz>
2024-05-01 18:17:18 +1200

    mock db structure #9

--- test/mock_db.js
+++ test/mock_db.js
@@ -1,3 +1,24 @@
-module.exports = {
-    
+
+async function getRooms() {
+    return [{ room_id: 0, room_name: 'Room0', threshold: 40 }, { room_id: 1, room_name: 'Room1', threshold: 20 }];
 }
+async function getOccupancy() {
+    return [{ room_id: 0, occupancy: [{ time: '2024-12-31T20:00:00.000Z', value: 10 }] }, { room_id: 1, occupancy: [{ time: '2024-12-31T20:00:00.000Z', value: 12 }] }];
+}
+
+async function getRoomById() {
+    return { room_id: 0, room_name: 'Room0', threshold: 40 };
+}
+
+async function getOccupancyByRoomId() {
+    return [{ time: '2024-12-31T20:00:00.000Z', value: 10 }];
+}
+
+const dao = {
+    getRooms,
+    getOccupancy,
+    getRoomById,
+    getOccupancyByRoomId
+};
+
+module.exports = dao;
