5a8bf1cff5058ac159226b72330bcdad0de692f3
Xavier Nuttall <nutxa216@student.otago.ac.nz>
2024-05-01 22:53:12 +1200

    sensor simulate some updates

--- backend/api/sensor_sim.js
+++ backend/api/sensor_sim.js
@@ -22,20 +22,20 @@ function generateRandomFunction() {
 
 function generateDay(startTime, endTime, steps) {
     const data = [];
-    const N = 1/steps; // out x values
+    const N = 1 / steps; // out x values
     const duration = (endTime - startTime)
     const randomFunction = generateRandomFunction();
 
 
 
     for (let i = 0; i < steps + 1; i++) {
-        data.push({ time: startTime + duration * i * N, value: randomFunction(N*i * Math.PI) });
+        data.push({ time: startTime + duration * i * N, value: randomFunction(N * i * Math.PI) });
     }
 
     const endValue = data[data.length - 1].value;
     // subtract off x*endValue from each and take the absolute value
     for (let i = 0; i < data.length; i++) {
-        data[i].value = Math.round(Math.abs(data[i].value-i*N*endValue) * 20);
+        data[i].value = Math.round(Math.abs(data[i].value - i * N * endValue) * 20);
     }
 
     return data;
@@ -44,12 +44,12 @@ function generateDay(startTime, endTime, steps) {
 async function generateRoom(days, startTime, endTime, steps, name) {
     const threshold = Math.floor(Math.random() * 20 + 4);
     // add the room to the database
-    ret = await fetch('http://localhost:3001/api/rooms',{
+    ret = await fetch('http://localhost:3001/api/rooms', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({room_name: name, threshold: threshold})
+        body: JSON.stringify({ room_name: name, threshold: threshold })
     })
 
     // get the body of the fetch back and save it as room id
@@ -60,13 +60,14 @@ async function generateRoom(days, startTime, endTime, steps, name) {
     // generate the data for each day
     for (let i = 0; i < days; i++) {
         const data = generateDay(startTime + i * 24 * 60 * 60 * 1000, endTime + i * 24 * 60 * 60 * 1000, steps);
-        for (j in data){
-            ret = await fetch('http://localhost:3001/api/rooms/occupancy',{
+        for (let j = 0; j < data.length; j++) {
+
+            fetch('http://localhost:3001/api/rooms/occupancy', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({room_id: rId, time: data[j].time, occupancy: data[j].value})
+                body: JSON.stringify({ room_id: rId, time: data[j].time, occupancy: data[j].value })
             })
         }
 
@@ -74,8 +75,30 @@ async function generateRoom(days, startTime, endTime, steps, name) {
 
 }
 
-generateRoom(days, startTime, endTime, steps, 'Room 1')
-// generateRoom()
+
+for (let i = 1; i < 11; i++) {
+    setTimeout(() => {
+        generateRoom(days, startTime, endTime, steps, 'Room ' + i)
+    }, 50 * i)
+}
+
+setTimeout(() => {
+    data = generateDay(endTime + 1000 * 60, endTime + 1000 * 60 * 30, 20)
+
+    for (let i = 0; i < data.length; i++) {
+        //wait 2s
+        setTimeout(() => {
+            console.log(data[i]);
+            fetch('http://localhost:3001/api/rooms/occupancy', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ room_id: 0, time: data[i].time, occupancy: data[i].value })
+            })
+        }, 2000 * i)
+    }
+}, 5000)
 
 
 
