d285f66aeb624271058a2b5538c847c8322191cc
temch584 <temch584@student.otago.ac.nz>
2024-05-01 16:39:01 +1200

    Added tests refs #9

--- test/test.js
+++ test/test.js
@@ -1,4 +1,67 @@
-test("fetching /", async () => {
-    let resp = await fetch("http://localhost:3001", {redirect: "manual"});
+test("fetching/", async () => {
+    let resp = await fetch("http://localhost:3001", { redirect: "manual" });
     expect(resp.status).toBe(302)
 })
+
+test("GetRooms/", async () => {
+    let resp = await fetch("http://localhost:3001/api/rooms", { redirect: "manual" });
+    let arr1 = [
+        {
+            room_id: 0,
+            room_name: "Room0",
+            threshold: 40
+        },
+        {
+            room_id: 1,
+            room_name: "Room1",
+            threshold: 20
+        },
+    ]
+    expect(resp.status).toBe(arr1);
+});
+
+test("GetRoomsOccupancy", async () => {
+    let resp = await fetch("http://localhost:3001/api/rooms/occupancy", { redirect: "manual" });
+    let arr1 = [
+        {
+            room_id: 0,
+            occupancy: [
+                {
+                    time: "2024-12-31T20:00:00.000Z",
+                    value: 10
+                }
+            ]
+        },
+        {
+            room_id: 1,
+            occupancy: [
+                {
+                    time: "2024-12-31T20:00:00.000Z",
+                    value: 12
+                }
+            ]
+        },
+    ]
+    expect(resp.status).toBe(arr1);
+});
+
+test("GetSpesificRoom/", async () => {
+    let resp = await fetch("http://localhost:3001/api/rooms/0", { redirect: "manual" });
+    let arr1 = {
+        room_id: 0,
+        room_name: "Room0",
+        threshold: 40
+    };
+    expect(resp.status).toBe(arr1);
+});
+
+test("GetSpesificRoomsOccupancy", async () => {
+    let resp = await fetch("http://localhost:3001/api/rooms/0/occupancy", { redirect: "manual" });
+    let arr1 = [
+        {
+            time: "2024-12-31T20:00:00.000Z",
+            value: 10
+        }
+    ]
+    expect(resp.status).toBe(arr1);
+});
